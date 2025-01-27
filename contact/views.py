fcc907622ad4d113f92c6b2141b6a4be155f7cef
fatemeee33 <fatemebagheri98@gmail.com>
2025-01-27 17:21:13 +0330

    Initial commit

---
+++ contact/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+
+# Create your views here.
+# views.py
+from django.shortcuts import render, HttpResponse
+from .forms import ContactForm
+
+
+def contact(request):
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+
+        if form.is_valid():
+            return HttpResponse("Yay! you are human.")
+        else:
+            return HttpResponse("OOPS! Bot suspected.")
+
+    else:
+        form = ContactForm()
+
+    return render(request, 'contact.html', {'form': form})
